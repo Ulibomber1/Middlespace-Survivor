ef8d075ceaff47bc9e8c4405579422bc1ea7cac3
dbickel8256 <daniel.bickel8256@coyote.csusb.edu>
2023-04-07 20:52:06 -0700

    Bullets Update
    Gamestate subscription moved to entity controller and player now fires bullets intermittently.

--- Assets/Scripts/Controllers/BulletController.cs
+++ Assets/Scripts/Controllers/BulletController.cs
@@ -2,13 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum ProjectyleType {PLAYER1, PLAYER2, ENEMY1, ENEMY2}
-
 public class BulletController : MonoBehaviour
 {
     [SerializeField] protected float damage;
     [SerializeField] protected float speed;
+    [SerializeField] protected float maxDespawn;
+    [SerializeField] protected float countdown;
+    [SerializeField] protected bool isPlaying = true;
+    [SerializeField] GameObject bulletSpawn;
     protected Rigidbody bulletRigidBody;
+
+    protected void GameStateChange()
+    {
+        switch (GameManager.Instance.gameState)
+        {
+            case GameState.PLAYING:
+                if (!isPlaying)
+                    isPlaying = true;
+                    bulletRigidBody.AddForce(transform.forward * speed);
+                break;
+            case GameState.PAUSE_MENU:
+            case GameState.LEVELED_UP:
+            case GameState.BUYING_EQUIPMENT:
+                if (isPlaying)
+                    isPlaying = false;
+                    bulletRigidBody.AddForce(-(transform.forward * speed));
+                break;
+            case GameState.MAIN_MENU:
+                DestroyBullet();
+                break;
+            case GameState.GAME_OVER:
+                isPlaying = false;
+                break;
+        }
+
+    }
+
+    protected void Start()
+    {
+        GameManager.Instance.OnStateChange += GameStateChange;
+        countdown = maxDespawn;
+    }
 
     protected void Awake()
     {
@@ -17,8 +51,20 @@ public class BulletController : MonoBehaviour
 
     protected void OnEnable()
     {
-        bulletRigidBody.AddForce(transform.up * speed);
-        Invoke("DestroyBullet", 5f);
+        bulletRigidBody.AddForce(transform.forward * speed);
+    }
+
+    protected void Update()
+    {
+        if (GameManager.Instance.gameState == GameState.PLAYING)
+        {
+            countdown -= Time.deltaTime;
+
+            if (countdown <= 0)
+            {
+                DestroyBullet();
+            }
+        }
     }
 
     protected void DestroyBullet()
--- Assets/Scripts/Controllers/EntityController.cs
+++ Assets/Scripts/Controllers/EntityController.cs
@@ -2,13 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EntityController : MonoBehaviour
+public class EntityController : MonoBehaviour, IDamageable
 {
     [SerializeField] protected float hitPoints;
     [SerializeField] protected float maxHitPoints;
     [SerializeField] protected float acceleration;
     [SerializeField] protected float maxVelocity;
     [SerializeField] protected float haltingDrag;
+    [SerializeField] protected float maxShotCooldown;
+    [SerializeField] protected float shotCoodown;
+    [SerializeField] protected bool isPlaying = true;
+
+    // IDamageable Implementations
+    float IDamageable.hitPoints { get { return hitPoints; } set { hitPoints = value; } }
+    public float damageResistance { get; }
+    public float healthRegenFactor { get; }
+    public delegate void PlayerDeadHandler();
+    public static event PlayerDeadHandler OnPlayerDead;
+    public void InflictDamage(float rawDamage)
+    {
+        hitPoints -= (1 - damageResistance) * rawDamage;
+        if (hitPoints <= 0.0f)
+        {
+            // Broadcast PlayerDead event
+            OnPlayerDead?.Invoke();
+        }
+    }
+    public void Heal(float healAmount)
+    {
+        hitPoints += healthRegenFactor * healAmount;
+    }
+    // IDamageable Implementations end
+
+    protected void GameStateChange()
+    {
+        switch (GameManager.Instance.gameState)
+        {
+            case GameState.PLAYING:
+                if (!isPlaying)
+                    isPlaying = true;
+                break;
+            case GameState.PAUSE_MENU:
+            case GameState.LEVELED_UP:
+            case GameState.BUYING_EQUIPMENT:
+                if (isPlaying)
+                    isPlaying = false;
+                break;
+            case GameState.MAIN_MENU:
+                Destroy(gameObject);
+                break;
+            case GameState.GAME_OVER:
+                isPlaying = false;
+                break;
+        }
+
+    }
 
     virtual protected void MoveEntity() { }
 }
