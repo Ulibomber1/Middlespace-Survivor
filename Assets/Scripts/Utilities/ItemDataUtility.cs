3efc5c01a805808b28060b6251004e554e9575d0
dbickel8256 <daniel.bickel8256@coyote.csusb.edu>
2023-05-06 12:58:04 -0700

    Equipment Update
    - Data for equipment has been added.
    - Data Handler adjusted for both items and equipment.

--- Assets/Scripts/Utilities/ItemDataUtility.cs
+++ Assets/Scripts/Utilities/ItemDataUtility.cs
@@ -16,6 +16,14 @@ public class ItemDataUtility : MonoBehaviour
             { "Magnet", "Magnetizes your chasey.  Increase pickup range." } };
     private List<string> ItemNames;
 
+    private Dictionary<string, int> EquipmentLevels;
+    private static readonly Dictionary<string, string> EquipmentBlurbs =
+        new Dictionary<string, string> {
+            { "Sword", "It says that its not a real sword, but dont tell your enemies that."},
+            { "Laser Pointer", "Given enough energy, you can cause damage with light!" },
+            { "Portable Plug", "Bluetooth plug gives you energy to move faster! Ahh, the wonders of technology!" } };
+    private List<string> EquipmentNames;
+
     private void Awake()
     {
         ItemLevels = new Dictionary<string, int>();
@@ -31,7 +39,16 @@ public class ItemDataUtility : MonoBehaviour
         ItemNames.Add("Battery");
         ItemNames.Add("Magnifier");
         ItemNames.Add("Magnet");
-        //for (int i = 0; i < Item)
+
+        EquipmentLevels = new Dictionary<string, int>();
+        EquipmentLevels.Add("Sword", 0);
+        EquipmentLevels.Add("Laser Pointer", 0);
+        EquipmentLevels.Add("Portable Plug", 0);
+
+        EquipmentNames = new List<string>();
+        EquipmentNames.Add("Sword");
+        EquipmentNames.Add("Laser Pointer");
+        EquipmentNames.Add("Portable Plug");
 
         LevelUpUtility.OnItemSelected += LevelUpItem;
     }
@@ -40,20 +57,39 @@ public class ItemDataUtility : MonoBehaviour
     public static event DataChangeHandler OnDataChange;
     private void LevelUpItem(string name)
     {
-        ItemLevels[name]++;
-        OnDataChange?.Invoke(name, ItemLevels[name]);
+        if (ItemLevels.ContainsKey(name))
+        {
+            ItemLevels[name]++;
+            OnDataChange?.Invoke(name, ItemLevels[name]);
+        }
+        else
+        {
+            EquipmentLevels[name]++;
+            OnDataChange?.Invoke(name, EquipmentLevels[name]);
+        }
+
     }
     public string GetItemBlurb(string name)
     {
-        return ItemBlurbs[name];
+        if (ItemLevels.ContainsKey(name))
+            return ItemBlurbs[name];
+        else
+            return EquipmentBlurbs[name];
     }
     public int GetItemLevel(string name)
     {
-        return ItemLevels[name];
+        if (ItemLevels.ContainsKey(name))
+            return ItemLevels[name];
+        else
+            return EquipmentLevels[name];
     }
     public string GetItemNameByIndex(int index)
     {
         return ItemNames[index];
+    }
+    public string GetEquipmentNameByIndex(int index)
+    {
+        return EquipmentNames[index];
     }
     public List<string> RandomItemIndices()
     {
@@ -73,6 +109,21 @@ public class ItemDataUtility : MonoBehaviour
         names.Add(ItemNames[temp]);
         Debug.Log("Indices (names):" + names[0]);
         Debug.Log("Indices (names):" + names[2]);
+        return names;
+    }
+
+    public List<string> RandomEquipmentIndices()
+    {
+        List<string> names = new List<string>();
+
+        int temp = Random.Range(0, EquipmentNames.Count - 1);
+        names.Add(EquipmentNames[temp]);
+        while (EquipmentNames[temp] == names[0])
+        {
+            temp = Random.Range(0, EquipmentNames.Count - 1);
+        }
+        names.Add(EquipmentNames[temp]);
+
         return names;
     }
 }
