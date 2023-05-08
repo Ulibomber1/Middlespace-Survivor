a670382261457282426bd0bf07eb94b53ab968b2
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-05-07 17:40:13 -0700

    Credit Getter for Unlock Screen

---
+++ Assets/Scripts/Utilities/CreditGetter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CreditGetter : MonoBehaviour
+{
+    TextMeshProUGUI textMesh;
+
+    private void Awake()
+    {
+        GameManager.Instance.OnCreditsUpdated += UpdateText;
+        textMesh = GetComponent<TextMeshProUGUI>();
+        textMesh.text = $"$: {GameManager.Instance.GetCurrentCredits()}";
+    }
+    
+
+
+    private void UpdateText(int credits)
+    {
+        textMesh.text = $"$: {credits}";
+    }
+}
