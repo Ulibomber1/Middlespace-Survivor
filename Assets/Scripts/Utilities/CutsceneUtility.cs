53bfcfad6b6dcc9b0771371b14038407cb1537c9
dbickel8256 <daniel.bickel8256@coyote.csusb.edu>
2023-05-09 18:52:26 -0700

    Small Fixes
    Player 2 does not spawn on player 1 death

--- Assets/Scripts/Utilities/CutsceneUtility.cs
+++ Assets/Scripts/Utilities/CutsceneUtility.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CutsceenUtility : MonoBehaviour
+public class CutsceneUtility : MonoBehaviour
 {
     float timer;
 
