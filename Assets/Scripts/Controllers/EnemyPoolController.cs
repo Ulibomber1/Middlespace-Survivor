f490381b5572a89751533e11fa98c96cda747fc5
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-06-09 16:24:36 -0700

    EnemyPoolController Organized

--- Assets/Scripts/Controllers/EnemyPoolController.cs
+++ Assets/Scripts/Controllers/EnemyPoolController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class EnemyPoolController : MonoBehaviour
 {
-    [SerializeField] private int poolNum;
-
+    //Public
     public delegate void OnAwake(GameObject pool, int poolNumber);
     public static event OnAwake onAwake;
+
+    //Private
+    [SerializeField] private int poolNum;
+
     private void Awake()
     {
         onAwake?.Invoke(this.gameObject, poolNum - 1);
