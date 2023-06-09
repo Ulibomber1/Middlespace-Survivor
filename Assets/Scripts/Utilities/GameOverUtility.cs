62fa6a57ce010b63602acd85114373cb2c5c8be4
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-06-09 16:41:28 -0700

    GameOverUtility Organized

--- Assets/Scripts/Utilities/GameOverUtility.cs
+++ Assets/Scripts/Utilities/GameOverUtility.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class GameOverUtility : MonoBehaviour
 {
+    //Public
     public delegate void GameOverUIAwakeHandler(GameObject GameOverUI);
     public static event GameOverUIAwakeHandler OnGameOverUIAwake;
 
+    //Private
     private void Awake()
     {
         OnGameOverUIAwake?.Invoke(gameObject);
