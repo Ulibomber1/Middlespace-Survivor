9b9832f83000f0b76219675db577f29935df0d8b
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-06-09 16:09:14 -0700

    EnemyBulletController Organized

--- Assets/Scripts/Controllers/EnemyBulletController.cs
+++ Assets/Scripts/Controllers/EnemyBulletController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class EnemyBulletController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Public
+
+    //Private
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         
