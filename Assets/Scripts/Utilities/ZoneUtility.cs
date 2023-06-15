af8c94a53bd273f135452243209fd072c2d2550b
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-06-15 13:04:43 -0700

    ZoneUtility Organized

--- Assets/Scripts/Utilities/ZoneUtility.cs
+++ Assets/Scripts/Utilities/ZoneUtility.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Global Enums
+
 public class ZoneUtility : MonoBehaviour
 {
+    // Private Enums
+
+    // References
+
+    // Internal Variables
+
+    // User-Defined Objects
+
+    // Delegates
     public delegate void OnStart(GameObject zone);
+
+    // Events
     public static event OnStart onStart;
+
+    // Unity Methods
     private void Start()
     {
         onStart?.Invoke(gameObject);
     }
+
+    // User-Defined Methods
 }
